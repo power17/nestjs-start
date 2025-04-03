bf3d6151bcbad61d2c098b7988f74b4a66f94d2f
power <liquanpower1@gmail.com>
2025-04-03 08:43:07 +0800

    feat：新增swc

--- src/common/logger/createRotateTransport.ts
+++ src/common/logger/createRotateTransport.ts
@@ -1,4 +1,4 @@
-import * as DailyRotateFile from 'winston-daily-rotate-file';
+import DailyRotateFile from 'winston-daily-rotate-file';
 import * as winston from 'winston';
 import { Console } from 'winston/lib/winston/transports';
 import { utilities } from 'nest-winston';
